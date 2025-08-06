6467d396ad6bbc72d92a5109b0cc82850e3b28ed
Groot14Babu92 <102485779+Groot14Babu92@users.noreply.github.com>
2025-08-06 17:39:10 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input("Enter a Number: "))
+
+sqrt_num = math.sqrt(num)
+print(f"The square root of {num} is: {sqrt_num}")
+
+natural_log = math.log(num)
+print(f"The natural logarithm of {num} is: {natural_log}")
+
+sine_num = math.sin(num)
+print(f"The sine of {num} is: {sine_num}")
